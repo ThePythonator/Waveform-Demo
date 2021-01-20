5a25aa298891d063b43346fa445fbcb1e0951260
ThePythonator <captainjackhappy@swampie.org.uk>
2021-01-20 12:01:04 +0000

    Basic template finished. Now I actually need to make the sounds.

--- game.cpp
+++ game.cpp
@@ -9,8 +9,12 @@ using namespace blit;
 // setup your game here
 //
 void init() {
-    set_screen_mode(ScreenMode::hires);
+    set_screen_mode(ScreenMode::lores);
 }
+
+// current_sound determines which sound is currently playing.
+// -1 indicates no sound.
+int current_sound = -1;
 
 ///////////////////////////////////////////////////////////////////////////
 //
@@ -21,16 +25,28 @@ void init() {
 //
 void render(uint32_t time) {
 
-    // clear the screen -- screen is a reference to the frame buffer and can be used to draw all things with the 32blit
+    // Clear the screen.
     screen.clear();
 
-    // draw some text at the top of the screen
+    // Draw some text ont the screen.
     screen.alpha = 255;
     screen.mask = nullptr;
     screen.pen = Pen(255, 255, 255);
-    screen.rectangle(Rect(0, 0, 320, 14));
+    screen.rectangle(Rect(0, 0, 160, 14));
+    screen.text("Press a button to", minimal_font, Point(5, 24));
+    screen.text("play a sound", minimal_font, Point(5, 32));
+
+    if (current_sound != -1) {
+        // A sound is playing currently.
+        // Display text indicating which sound.
+
+        std::string text = "Current sound: ";
+        text.append(std::to_string(current_sound));
+        screen.text(text, minimal_font, Point(5, 48));
+    }
+
     screen.pen = Pen(0, 0, 0);
-    screen.text("Hello 32blit!", minimal_font, Point(5, 4));
+    screen.text("Waveform Demo", minimal_font, Point(5, 4));
 }
 
 ///////////////////////////////////////////////////////////////////////////
@@ -41,4 +57,62 @@ void render(uint32_t time) {
 // amount if milliseconds elapsed since the start of your game
 //
 void update(uint32_t time) {
+    // Generate a sound if a button is pressed.
+    //
+    // Only detect one button press at a time (by using else if) because reasons.
+
+    if (buttons.pressed & Button::X) {
+        current_sound = 0;
+    }
+    else if (buttons.pressed & Button::A) {
+        current_sound = 1;
+    }
+    else if (buttons.pressed & Button::B) {
+        current_sound = 2;
+    }
+    else if (buttons.pressed & Button::Y) {
+        current_sound = 3;
+    }
+    else if (buttons.pressed & Button::DPAD_UP) {
+        current_sound = 4;
+    }
+    else if (buttons.pressed & Button::DPAD_RIGHT) {
+        current_sound = 5;
+    }
+    else if (buttons.pressed & Button::DPAD_DOWN) {
+        current_sound = 6;
+    }
+    else if (buttons.pressed & Button::DPAD_LEFT) {
+        current_sound = 7;
+    }
+
+    // Update the sounds which are playing.
+    switch (current_sound) {
+    case 0:
+        break;
+
+    case 1:
+        break;
+
+    case 2:
+        break;
+
+    case 3:
+        break;
+
+    case 4:
+        break;
+
+    case 5:
+        break;
+
+    case 6:
+        break;
+
+    case 7:
+        break;
+
+    default:
+        break;
+    }
 }
